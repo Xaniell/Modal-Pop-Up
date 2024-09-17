67da95ff73f8c00ecf85b1b43dba7e64beb44ad9
Xaniell <ezetobechukwu06@gmail.com>
2024-09-17 23:48:01 +0100

    finished the html and css for the modal

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let thesumbit = document.querySelector(".submit");
+let nameInput = document.querySelector(".nameInput");
+let nameSpan = document.querySelector(".nameSpan");
+
+document.querySelector(".submit").addEventListener("click", function () {
+
+})
