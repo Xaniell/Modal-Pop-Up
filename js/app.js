43d18fb8fbf16e72214a5eb1c6179f1c27bcfb99
Xaniell <ezetobechukwu06@gmail.com>
2024-09-18 01:20:45 +0100

    finished the modal page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 let thesumbit = document.querySelector(".submit");
 let nameInput = document.querySelector(".nameInput");
 let nameSpan = document.querySelector(".nameSpan");
+let theclose = document.querySelector(".close");
 
 document.querySelector(".submit").addEventListener("click", function () {
+  let thename = nameInput.value;
+  nameSpan.innerText = thename;
+  document.querySelector(".modalPop").style.display = "flex";
+//   document.querySelector(".welcomeNote").style.display = "none";
+  // alert("Welcome " + thename);
+  nameInput.value = "";
+});
 
-})
+theclose.addEventListener("click", function () {
+  document.querySelector(".modalPop").style.display = "none";
+  document.querySelector(".welcomeNote").style.display = "flex";
+});
+
